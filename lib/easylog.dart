dd0039b2b936def258d5ba37dbd1b16f1e152484
swaraj <sswaraj169@gmail.com>
2023-06-22 20:21:08 +0530

    Bug Fixed

--- lib/easylog.dart
+++
@@ -1,3 +0,0 @@
-library flutter_logger_plus;
-
-export 'src/flutter_logger_plus.dart';
--- lib/flutter_logger_plus.dart
+++ lib/flutter_logger_plus.dart
@@ -1,7 +1,3 @@
 library flutter_logger_plus;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/flutter_logger_plus.dart';
